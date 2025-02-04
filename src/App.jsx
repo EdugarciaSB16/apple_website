72f8976c59117ceef60c6d7c3413d08a1f2d9e83
Eduardo Garcia <edugarciasb16@gmail.com>
2025-02-04 02:10:03 +0100

    feat: add new model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
             <Highlights />
+            <Model />
         </main>
     );
 };
